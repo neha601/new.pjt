f94280be8a0487017c5b81b9e5826c9cb5a39ebf
neha601 <nehuejaz@gmail.com>
2024-09-11 11:01:54 -0700

    update

--- src/app/address/page.tsx
+++ src/app/address/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 const page = () => {
   return(
-    <div>Home page</div>
+    <div>New Gulberg Garden RYK</div>
   )
 }
 
--- src/app/name/page.tsx
+++ src/app/name/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 const page = () => {
   return(
-    <div>Home page</div>
+    <div>Neha Ejaz Ahmed</div>
   )
 }
 
